acb66d760a6aecb90957c2c7247714a04686687c
iamaparnarajesh <aparna.sri85@gmail.com>
2020-07-28 16:00:15 +0800

    studentrecord details

---
+++ blockchain/studentrecord/migrations/1_initial_migration.js
@@ -0,0 +1,8 @@
+const Migrations = artifacts.require("Migrations");
+const StudentRecord = artifacts.require("StudentRecord");
+
+
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
+  deployer.deploy(StudentRecord);
+};
